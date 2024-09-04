4aa8bd4b0eb91e6841693be78097aa4e2ad77638
joaogabrielsantosD <joao.donato@ufpe.br>
2024-09-03 21:12:44 -0300

    Macro definitions

---
+++ definitions.h
@@ -0,0 +1,16 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+/* GPIOs */
+#define EVENT_1 4 // PB4
+#define EVENT_2 3 // PB3
+#define EVENT_3 2 // PB2
+#define EVENT_4 1 // PB1
+
+/* Events */
+#define PEDESTRE              EVENT_1
+#define CARRO_SECUNDARIA      EVENT_2
+#define AMBULANCIA_PRINCIPAL  EVENT_3
+#define AMBULANCIA_SECUNDARIA EVENT_4
+
+#endif
